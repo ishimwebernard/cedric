00fd96a2ac00e13082ea02bd6daac0b04f845426
ishimwebernard <isbernard2001@gmail.com>
2023-10-26 21:46:27 +0200

    mend

--- controllers/users.js
+++ controllers/users.js
@@ -16,7 +16,7 @@ module.exports = {
                 Email: req.body.Email,
             }
         }).then((users) => {
-            if (!users) return res.status(400).send({message: "User not found"})
+            if (users.length == 0) return res.status(400).send({message: "User not found"})
 
             if ( users[0].Password == req.body.Password)
             return res.status(200).send({message: "Login Succesful"})
